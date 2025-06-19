913f40ad22d6dd646b56ae4d7962de1125801d7d
Samuel Silva <samuel7silva@outlook.com>
2025-06-19 13:45:49 -0300

    feat: Adds Google authentication route and user handling
    Implements a new route for Google authentication, allowing users to sign in with their Google accounts.
    
    If a user does not exist, it creates a new user with a hashed Google ID as the password. If the user exists without a password, it updates their record with the hashed Google ID.
    
    Additionally, it validates credentials and returns a JWT token upon successful authentication.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,16 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
-import { compare } from 'bcrypt'
+import { compare, hash } from 'bcrypt'
 
 interface AuthUserRequest {
   email: string
   password: string
+}
+
+interface AuthGoogleRequest {
+  email: string
+  name: string
+  googleId: string
 }
 
 export async function authRoutes(app: FastifyInstance) {
@@ -43,6 +49,66 @@ export async function authRoutes(app: FastifyInstance) {
       name: userAlreadyExists.name,
       sub: userAlreadyExists.id,
       token,
+    }
+  })
+
+  app.post('/auth/google', async (request, reply) => {
+    try {
+      // Valida os dados recebidos do Google via Next-auth
+      const { email, name, googleId } = request.body as AuthGoogleRequest
+      console.log('Dados recebidos do Google:', { email, name, googleId })
+
+      const existingUser = await prisma.user.findFirst({
+        where: {
+          email,
+        },
+      })
+
+      let user = existingUser
+
+      if (!existingUser) {
+        const passwordHash = await hash(googleId, 8)
+        user = await prisma.user.create({
+          data: {
+            email,
+            name,
+            role: 'USER',
+            password: passwordHash,
+          },
+        })
+      } else if (!existingUser.password) {
+        const passwordHash = await hash(googleId, 8)
+
+        user = await prisma.user.update({
+          where: { id: existingUser.id },
+          data: { password: passwordHash },
+        })
+      } else if (existingUser) {
+        const passwordMatch = await compare(googleId, existingUser?.password)
+        if (!passwordMatch) {
+          reply.code(401).send({ message: 'Usuário ou senha incorretos' })
+          return
+        }
+      }
+
+      const token = app.jwt.sign(
+        {
+          name: user?.name,
+          role: user?.role,
+        },
+        {
+          sub: user?.id,
+          expiresIn: '7 days',
+        },
+      )
+
+      return {
+        name: user?.name,
+        sub: user?.id,
+        token,
+      }
+    } catch (error) {
+      reply.code(400).send({ message: 'Erro ao autenticar com Google' })
     }
   })
 }
